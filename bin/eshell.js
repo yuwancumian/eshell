5b67d1a9789a576c1655d68d19311415356c1511
yuwancumian <easier2005@163.com>
2015-10-10 16:30:45 +0800

    v0.1.0

---
+++ bin/eshell.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+require('shelljs/global');
+var pkg = require('../lib/package.json');
+
+var shellScript = process.argv[2];
+var scriptName = process.argv[3];
+
+
+mkdir('-p',shellScript);
+cd(shellScript);
+JSON.stringify(pkg, null, 4).to('package.json');
+"### Readme".to('README.md')
+
+//sed('-i','shellScript', shellScript, 'package.json');
+//sed('-i','scriptName', scriptName, 'package.json');
+
+
+mkdir('-p','bin');
+mkdir('-p','lib');
+cd('bin');
+"#!/usr/bin/env node\n".to(scriptName + '.js');
+"require('shelljs/global');".toEnd(scriptName + '.js');
+echo(shellScript + ' was created!');
